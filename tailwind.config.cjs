800057ab595148c4a6a48f2cddb9504f39a0058e
Jared Truscott <jtruscott78@gmail.com>
2023-04-22 15:53:06 -0400

    created PDP

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -22,6 +22,7 @@ module.exports = {
 	},
 	plugins: [
 		require('@tailwindcss/aspect-ratio'),
-		require('@tailwindcss/forms')
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/typography')
 	],
 }
